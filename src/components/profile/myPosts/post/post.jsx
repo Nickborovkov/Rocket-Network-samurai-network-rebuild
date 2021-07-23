26d7a1a8c9ad6ce3ed961f86216e3981ca7759dc
Nikita Borovkov <borovckov@yandex.ru>
2021-07-24 00:21:02 +0300

    post delete button disabled when loading unauthorised profile

--- src/components/profile/myPosts/post/post.jsx
+++ src/components/profile/myPosts/post/post.jsx
@@ -4,11 +4,11 @@ import styles from '../myPosts.module.css'
 import { VscClose } from 'react-icons/vsc';
 import { AiOutlineLike } from 'react-icons/ai';
 
-const Post = ({post, deletePost}) => {
+const Post = ({post, deletePost, isOwner, profile}) => {
     return <div className={styles.post}>
         <div className={styles.postAvatarHolder}>
             <img className={styles.postAvatar}
-                 src={defaultAvatar}
+                 src={profile.photos.small || defaultAvatar}
                  alt="postAvatar"/>
         </div>
 
@@ -18,9 +18,12 @@ const Post = ({post, deletePost}) => {
             <p className={styles.postLikes}>{post.likescount}</p>
             <button className={styles.postLikeButton}><AiOutlineLike/></button>
         </div>
+        {
+            isOwner &&
+            <button className={styles.deleteButton}
+                    onClick={ () => {deletePost(post.id)} }><VscClose/></button>
+        }
 
-        <button className={styles.deleteButton}
-                onClick={ () => {deletePost(post.id)} }><VscClose/></button>
     </div>
 }
 
