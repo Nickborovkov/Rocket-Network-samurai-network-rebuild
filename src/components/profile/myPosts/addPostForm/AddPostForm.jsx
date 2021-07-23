f188b471e4dc1a518c408035311cb84695b62ce2
Nikita Borovkov <borovckov@yandex.ru>
2021-07-24 00:18:32 +0300

    addPost form moved to separate component, added validators, textarea replaced with smart one

--- src/components/profile/myPosts/addPostForm/AddPostForm.jsx
+++ src/components/profile/myPosts/addPostForm/AddPostForm.jsx
@@ -1,6 +1,10 @@
 import React from "react";
 import {Field, reduxForm} from "redux-form";
 import styles from '../myPosts.module.css'
+import {maxLengthCreator, required} from "../../../../utils/formHelpers/validators";
+import {TextArea} from "../../../../utils/formHelpers/formControls";
+
+let maxLength300 = maxLengthCreator(300)
 
 let AddPostForm = ({addPost}) => {
     let onAddPost = (value) => {
@@ -16,10 +20,11 @@ export default AddPostForm
 let AddPost = ({handleSubmit}) => {
     return <form onSubmit={handleSubmit}>
         <Field className={styles.textarea}
-               component='textarea'
+               component={TextArea}
                type='text'
                placeholder='Enter your post here...'
-               name='addUserPost'/>
+               name='addUserPost'
+               validate={[required, maxLength300]}/>
         <button className={styles.submitButton}>Post</button>
     </form>
 }
