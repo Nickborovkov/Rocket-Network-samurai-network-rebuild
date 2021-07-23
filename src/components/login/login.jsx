3f999e3c539de6312895618faf03ed710f724dbe
Nikita Borovkov <borovckov@yandex.ru>
2021-07-24 00:16:42 +0300

    added captha (when email or password are wrong for more than 10 times), added validators to inputs, added stopSubmit error showing (comes from server)

--- src/components/login/login.jsx
+++ src/components/login/login.jsx
@@ -2,11 +2,13 @@ import React from "react";
 import {Field, reduxForm} from "redux-form";
 import {Redirect} from "react-router-dom";
 import styles from './login.module.css'
+import {required} from "../../utils/formHelpers/validators";
+import {Input} from "../../utils/formHelpers/formControls";
 
-let Login = ({isAuth, loginNewUser}) => {
+let Login = ({isAuth, loginNewUser, captcha}) => {
 
     let onFormSubmit = (values) => {
-        loginNewUser(values.userLogin, values.userPassword, values.rememberMe)
+        loginNewUser(values.userLogin, values.userPassword, values.rememberMe, values.userCaptcha)
     }
 
     if (isAuth) return <Redirect to='/profile'/>
@@ -14,26 +16,28 @@ let Login = ({isAuth, loginNewUser}) => {
     return (
         <div>
             <h2 className={styles.title}>Login</h2>
-            <LoginFormRedux onSubmit={onFormSubmit}/>
+            <LoginFormRedux onSubmit={onFormSubmit} captcha={captcha}/>
         </div>
     )
 }
 
 export default Login
 
-let Loginform = (props) => {
+let Loginform = ({error, handleSubmit, captcha}) => {
     return <form className={styles.loginForm}
-                 onSubmit={props.handleSubmit}>
+                 onSubmit={handleSubmit}>
         <Field className={styles.input}
-               component='input'
+               component={Input}
                type='text'
                placeholder='Email...'
-               name='userLogin'/>
+               name='userLogin'
+               validate={[required]}/>
         <Field className={styles.input}
-               component='input'
+               component={Input}
                type='password'
                placeholder='Password...'
-               name='userPassword'/>
+               name='userPassword'
+               validate={[required]}/>
         <div className={styles.checkboxHolder}>
             <Field className={styles.checkbox}
                    component='input'
@@ -41,7 +45,19 @@ let Loginform = (props) => {
                    name='rememberMe'/>
             <p className={styles.checkBoxLabel}>Remember me?</p>
         </div>
-
+        {
+            captcha && <div>
+                <img src={captcha} alt="captcha" />
+                <Field className={styles.input}
+                       component = {Input}
+                       type='input'
+                       placeholder='Enter captcha...'
+                       name='userCaptcha'/>
+            </div>
+        }
+        {
+            error && <div className={styles.errorDiv}>{error}</div>
+        }
         <button className={styles.loginButton}>Login</button>
     </form>
 }
