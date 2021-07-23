47ce0fe4aae7f4d6469b3e6aeffb944f1d0f0007
Nikita Borovkov <borovckov@yandex.ru>
2021-07-24 00:03:55 +0300

    added 404 page

---
+++ src/utils/common/404/page404.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import styles from './page404.module.css'
+
+let Page404 = () => {
+    return <div>
+        <h2 className={styles.title}>404 page not found</h2>
+    </div>
+}
+
+export default Page404
