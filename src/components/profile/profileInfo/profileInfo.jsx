e74743de683aa187545d61bae87264f563ad6558
Nikita Borovkov <borovckov@yandex.ru>
2021-07-24 00:30:54 +0300

    Profile content moved to separate component, profileAvatar moved to separate component

--- src/components/profile/profileInfo/profileInfo.jsx
+++ src/components/profile/profileInfo/profileInfo.jsx
@@ -1,59 +1,26 @@
-import React, {useState} from "react";
-import defaultAvatar from '../../../utils/images/defaultAvatar.jpg'
+import React from "react";
 import Preloader from "../../../utils/common/preloader/Preloader";
 import styles from './profileInfo.module.css'
-import {AiOutlineLink} from 'react-icons/ai';
 import ProfileStatus from "./profileStatus/profileStatus";
-import cn from 'classnames'
-import {AiOutlineDownload} from 'react-icons/ai';
+import ProfileAvatar from "./avatarSection/profileAvatar";
+import ProfileContent from "./profileContent/profileContent";
 
 
-let ProfileInfo = ({profile, userStatus, updateUserStatus, isOwner, updateUserPhoto}) => {
-
-    let [downloadButton, setDownloadButton]= useState(false)
-    let showDownloadPhoto = () => {
-        setDownloadButton(true)
-    }
-    let hideDownloadPhoto = () => {
-        setDownloadButton(false)
-    }
-
-    let onMainPhotoChange = (e) => {
-        if (e.target.files.length) {
-            updateUserPhoto(e.target.files[0])
-        }
-
-    }
+let ProfileInfo = ({profile, userStatus, updateUserStatus,
+                       isOwner, updateUserPhoto, updateUserProfile}) => {
 
     if (!profile) {
         return <Preloader/>
     }
-    return <div className={styles.profileInfo}>
+
+    return <div>
         <div className={styles.profileInner}>
-            <div className={styles.avatarHolder}>
-                <img className={styles.avatar}
-                     src={profile.photos.large || defaultAvatar}
-                     alt="avatar"
-                />
-                {
-                    isOwner && <div className={styles.downloadHolder}>
-                        {
-                            downloadButton &&
-                            <label className={styles.downLoadButton}><AiOutlineDownload/>
-                                <input className={styles.photoInput} type='file'
-                                       name='fileInput'
-                                       onChange={onMainPhotoChange}/>
-                            </label>
-                        }
+            <ProfileAvatar updateUserPhoto={updateUserPhoto}
+                           profile={profile}
+                           isOwner={isOwner}/>
 
-
-
-                    </div>
-
-                }
-            </div>
             <div className={styles.mainInfoHolder}>
-                <div className={styles.infoInner}>
+                <div>
                     <div className={styles.userName}>{profile.fullName}</div>
 
                     <ProfileStatus userStatus={userStatus}
@@ -63,39 +30,12 @@ let ProfileInfo = ({profile, userStatus, updateUserStatus, isOwner, updateUserPh
                 </div>
             </div>
         </div>
-        <div className={styles.propsHolder}>
-            <div className={styles.infoRow}>
-                <h3 className={styles.infoSubtitle}>Info</h3>
-                <p className={styles.infoValue}>{profile.aboutMe}</p>
-            </div>
-            <div className={styles.infoRow}>
-                <h3 className={styles.infoSubtitle}>Professional Skills</h3>
-                <p className={styles.infoValue}>{profile.lookingForAJobDescription}</p>
-                {
-                    profile.lookingForAJob
-                        ? <p className={cn(styles.infoValue, styles.jobStatus)}>Looking for a job</p>
-                        : <p className={cn(styles.infoValue, styles.jobStatus)}>Not looking for a job</p>
-                }
-            </div>
-            <div className={styles.infoRow}>
-                <h3 className={styles.infoSubtitle}>Contacts</h3>
-                {
-                    Object.keys(profile.contacts).map(key => {
-                        if (!profile.contacts[key]) return null
-                        return <Contact key={key} contactTitle={key} contactValue={profile.contacts[key]}/>
-                    })
-                }
-            </div>
-        </div>
+
+        <ProfileContent profile={profile}
+                        updateUserProfile={updateUserProfile}/>
 
     </div>
 }
 
 export default ProfileInfo;
 
-let Contact = ({contactTitle, contactValue}) => {
-    return <div className={styles.linkHolder}>
-        <p className={styles.linkTitle}>{contactTitle}</p>
-        <a className={styles.link} href={contactValue}><AiOutlineLink/></a>
-    </div>
-}
