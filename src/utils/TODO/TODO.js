7b0b9b1f9a2113cb447e70734fb160c8e063ad48
Nickborovkov <borovckov@yandex.ru>
2021-07-27 17:44:25 +0300

    update TODO list

--- src/utils/TODO/TODO.js
+++ src/utils/TODO/TODO.js
@@ -1,5 +1,4 @@
-//TODO: `add filters on users list with reselect`
-//TODO: `add friends page (without code repeating) (try to use HOC)`
-//TODO: `fix reselect bugs (try to get all users with cycle (0 to totalUsersCount)) to make filters`
+//TODO: `add filters on users list with reselect (need to get all users list with for to make filters)`
+//TODO: `fix friends page reboot`
 //TODO: `make the app responsible (suitable for all screen sizes)`
 //TODO: `errors handling (global errors, local errors (try/catch))`
