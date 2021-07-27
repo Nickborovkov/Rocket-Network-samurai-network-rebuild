4d7082e4d2daa9dcae0c796bbffad7aacfa872b1
Nikita Borovkov <borovckov@yandex.ru>
2021-07-28 02:04:00 +0300

    repeated code replaced

--- src/components/profile/profileInfo/profileContent/profileContent.jsx
+++ src/components/profile/profileInfo/profileContent/profileContent.jsx
@@ -14,15 +14,28 @@ import { AiFillGithub } from 'react-icons/ai';
 import { FaUserAlt } from 'react-icons/fa';
 
 
-let ProfileContent = ({profile, updateUserProfile, isOwner}) => {
+const ProfileContent = ({profile, updateUserProfile, isOwner}) => {
 
-    let [editMode, setEditMode] = useState(false)
-    let activateEditMode = () => {
+    const [editMode, setEditMode] = useState(false)
+    const activateEditMode = () => {
         setEditMode(true)
     }
-    let deActivateEditMode = () => {
+    const deActivateEditMode = () => {
         setEditMode(false)
     }
+
+
+    const iconsArray = [<AiFillFacebook/>, <CgWebsite/>, <FaVk/>, <AiFillTwitterSquare/>,
+        <AiFillInstagram/>, <AiFillYoutube/>, <AiFillGithub/>, <FaUserAlt/>]
+
+    const result = []
+
+    for(let i = 1; i < iconsArray.length; i++){
+        result.push({id:iconsArray.indexOf(iconsArray[i]),
+            link: Object.values(profile.contacts)[i],
+            icon: iconsArray[i]})
+    }
+
 
     return <div className={styles.propsHolder}>
 
@@ -54,9 +67,10 @@ let ProfileContent = ({profile, updateUserProfile, isOwner}) => {
                         <h3 className={styles.infoSubtitle}>Contacts</h3>
                         <div className={styles.contactsList}>
                             {
-                                Object.keys(profile.contacts).map(key => {
-                                    if (!profile.contacts[key]) return null
-                                    return <Contact key={key} contactTitle={key} contactValue={profile.contacts[key]}/>
+                                result.map(contact => {
+                                    return <Contact key={contact.id}
+                                                    contactUrl={contact.link}
+                                                    contactIcon={contact.icon}/>
                                 })
                             }
                         </div>
@@ -71,29 +85,6 @@ let ProfileContent = ({profile, updateUserProfile, isOwner}) => {
 
 export default ProfileContent
 
-let Contact = ({contactTitle, contactValue}) => {
-    if(contactTitle === `facebook`){
-        return <a className={styles.link} href={contactValue}><AiFillFacebook/></a>
-    }
-    if(contactTitle === `website`){
-        return <a className={styles.link} href={contactValue}><CgWebsite/></a>
-    }
-    if(contactTitle === `vk`){
-        return <a className={styles.link} href={contactValue}><FaVk/></a>
-    }
-    if(contactTitle === `twitter`){
-        return <a className={styles.link} href={contactValue}><AiFillTwitterSquare/></a>
-    }
-    if(contactTitle === `instagram`){
-        return <a className={styles.link} href={contactValue}><AiFillInstagram/></a>
-    }
-    if(contactTitle === `youtube`){
-        return <a className={styles.link} href={contactValue}><AiFillYoutube/></a>
-    }
-    if(contactTitle === `github`){
-        return <a className={styles.link} href={contactValue}><AiFillGithub/></a>
-    }
-    if(contactTitle === `mainLink`){
-        return <a className={styles.link} href={contactValue}><FaUserAlt/></a>
-    }
+const Contact = ({contactUrl, contactIcon}) => {
+    return <a className={styles.link} href={contactUrl}>{contactIcon}</a>
 }
