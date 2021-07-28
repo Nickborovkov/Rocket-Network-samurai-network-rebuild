8667a737cc2ecfc7796ab4f8266c942a28fc80fc
Nikita Borovkov <72877821+Nickborovkov@users.noreply.github.com>
2021-07-29 02:15:39 +0300

    update TODO.js

--- src/utils/TODO/TODO.js
+++ src/utils/TODO/TODO.js
@@ -1,2 +1,3 @@
 //TODO: `make the app responsible (suitable for all screen sizes)`
 //TODO: `errors handling (global errors, local errors (try/catch))`
+//TODO: `add errors handling in profileContent edit`
